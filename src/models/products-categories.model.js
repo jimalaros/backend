982abf0b9d443051d839b9953a84832096fd902c
jimalaros <jimalaros25@gmail.com>
2022-10-15 09:09:52 -0500

    first commit

--- src/models/products-categories.model.js
+++ src/models/products-categories.model.js
@@ -4,7 +4,7 @@ class ProductsByCategory{
 
     static async getProductsByCategory(){
         return query(
-            "SELECT product.name AS nameProduct, product.url_image AS image, product.price AS price, category.name AS nameCategory FROM product, category WHERE product.category = category.id"
+            "SELECT product.id AS id, product.name AS nameProduct, product.url_image AS image, product.price AS price, category.name AS nameCategory FROM product, category WHERE product.category = category.id"
         )
     }
 }
